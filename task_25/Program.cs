471b24efb51cc3696280d4c943d236258bcb7e6a
Anastasia Novitskaya <nastasya11011995@gmail.com>
2023-03-20 14:14:37 +1200

    task_25

---
+++ task_25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+for (int i = 0; i < num2; i++)
+{
+    res *= num1;
+} 
+
+Console.WriteLine($"Результат возведения числа равен: {res}");
