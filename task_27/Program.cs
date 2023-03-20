12dbaa336387a71d2244e075c72f8d40e6138b23
Anastasia Novitskaya <nastasya11011995@gmail.com>
2023-03-20 20:26:10 +1200

    task_27

---
+++ task_27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (num > 0)
+{
+    sum += num % 10;
+    num /= 10;
+}
+Console.WriteLine($"Сумма всех цифр числа: {sum}");
